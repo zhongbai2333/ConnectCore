e7c20f0a6d6ece99b34e7ea539bc65866c145da9
zhong_bai <54356787+zhongbai2333@users.noreply.github.com>
2024-08-12 21:32:49 +0800

    Update: Add get_server_list api

--- connect_core/api/websocket/client/__init__.py
+++ connect_core/api/websocket/client/__init__.py
@@ -1,3 +1,7 @@
-from connect_core.websocket.websocket_client import websocket_client_init, get_server_id
+from connect_core.websocket.websocket_client import (
+    websocket_client_init,
+    get_server_id,
+    get_server_list,
+)
 
-__all__ = ["websocket_client_init", "get_server_id"]
+__all__ = ["websocket_client_init", "get_server_id", "get_server_list"]
